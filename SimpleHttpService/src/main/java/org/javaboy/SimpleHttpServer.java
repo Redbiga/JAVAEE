4968b1da4362de3435f501c70d6f16472e697141
RedA <3106607759@qq.com>
2023-11-30 11:21:34 +0800

    java实现了简单的http服务

---
+++ SimpleHttpService/src/main/java/org/javaboy/SimpleHttpServer.java
@@ -0,0 +1,31 @@
+package org.javaboy;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SimpleHttpServer {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        context.addServlet(new ServletHolder(new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                resp.setContentType("text/html");
+                resp.getWriter().write("<h1>Hello, World!</h1>");
+            }
+        }), "/*");
+
+        server.start();
+        server.join();
+    }
+}
