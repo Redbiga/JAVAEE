64f7b92b0b897f70ca5cd0434d171b8a25cade03
RedA <3106607759@qq.com>
2023-12-10 21:11:41 +0800

    java不需要tomcat实现的简单的http服务

--- SimpleHttpService/src/main/java/org/javaboy/SimpleHttpServer.java
+++ SimpleHttpService/src/main/java/org/javaboy/SimpleHttpServer.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * @author redA
+ */
 public class SimpleHttpServer {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
